02da075572c745e2e86e1a3ead22c41b21d16266
Dyllan McCreary <dyllanmccreary@gmail.com>
2018-04-11 08:36:22 -0700

    ! renamed to global.js: will only have non-site-specific functions

--- global.js
+++ global.js
@@ -1,7 +1,17 @@
+/*
+ *	Contains all features that are not site-specific.
+ */
+
+// TODO make these options changeable in the extension button drop-down
 let scrollMultiplier = 13;
 let scrollSprintMultiplier = 2;
 
 window.addEventListener( "rightanalogverticalpoll", function( e ) {
+	let val = e.detail.current;
+	if ( val < 0.15 && val > -0.15 ) {
+		return;
+	}
+
 	let m = e.detail.controller.buttons[11].pressed ? scrollMultiplier * scrollSprintMultiplier : scrollMultiplier;
-	window.scrollBy( 0, e.detail.current * m );
+	window.scrollBy( 0, val * m );
 } );
