baee43377a739d76a6d06816609b5e8b72fd34bc
Dyllan McCreary <dyllanmccreary@gmail.com>
2018-04-12 02:22:28 -0700

    + X button pauses/plays & holding Y displays the bottom bar

---
+++ js/youtube-videoplayer.js
@@ -0,0 +1,18 @@
+// X pauses and plays the video
+window.addEventListener( "xbuttonpressed", function() {
+	let playButton = $( ".ytp-play-button.ytp-button" );
+	playButton.trigger( "click" );
+} );
+
+// holding Y displays the bar at the bottom
+window.addEventListener( "ybuttonpoll", function() {
+	let vc = $( "div .html5-video-player" );
+	vc.removeClass( "ytp-autohide" );
+} );
+
+window.addEventListener( "ybuttonreleased", function() {
+	let vc = $( "div .html5-video-player" );
+	if ( !vc.hasClass( "ytp-autohide" ) ) {
+		vc.addClass( "ytp-autohide" );
+	}
+} );
