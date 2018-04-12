f688bf593977d350d2cd4a518dc91c95a10af447
Dyllan McCreary <dyllanmccreary@gmail.com>
2018-04-11 23:56:29 -0700

    + send message to background for zooming when analogs are in opp dirs

--- global.js
+++ global.js
@@ -26,3 +26,20 @@ window.addEventListener( "leftbumperpressed", function() {
 window.addEventListener( "rightbumperpressed", function() {
 	window.history.forward();
 } );
+
+// Zoom in and out
+window.addEventListener( "rightanaloghorizontalpoll", function( e ) {
+	let rightValue = e.detail.current;
+	let leftValue = e.detail.controller.axes[0];
+	let avg = ( rightValue * -1 + leftValue ) / 2;
+
+	if ( rightValue > 0.2 && leftValue < -0.2 ) {
+		// TODO move function to a utils file
+		console.log( avg );
+		chrome.runtime.sendMessage( {eventType: "requestzoom", factor: avg} );
+	} else if ( rightValue < -0.2 && leftValue > 0.2 ) {
+		// TODO move function to a utils file
+		console.log( avg );
+		chrome.runtime.sendMessage( {eventType: "requestzoom", factor: avg} );
+	}
+} );
