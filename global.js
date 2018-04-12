20d844fee76553d0f7e46f76ed90ea73b03f4923
Dyllan McCreary <dyllanmccreary@gmail.com>
2018-04-11 21:10:11 -0700

    + forward / backward through history using RB + B &  LB + B

--- global.js
+++ global.js
@@ -6,6 +6,7 @@
 let scrollMultiplier = 13;
 let scrollSprintMultiplier = 2;
 
+// Scrolling
 window.addEventListener( "rightanalogverticalpoll", function( e ) {
 	let val = e.detail.current;
 	if ( val < 0.15 && val > -0.15 ) {
@@ -14,4 +15,15 @@ window.addEventListener( "rightanalogverticalpoll", function( e ) {
 
 	let m = e.detail.controller.buttons[11].pressed ? scrollMultiplier * scrollSprintMultiplier : scrollMultiplier;
 	window.scrollBy( 0, val * m );
+} );
+
+// Forward / backwards through history
+window.addEventListener( "bbuttonpressed", function( e ) {
+	let leftBumper = e.detail.controller.buttons[4];
+	let rightBumper = e.detail.controller.buttons[5];
+	if ( leftBumper.pressed && !rightBumper.pressed ) {
+		window.history.back();
+	} else if ( rightBumper.pressed && !leftBumper.pressed ) {
+		window.history.forward();
+	}
 } );
