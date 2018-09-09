d2d1ce9c4cc282d10bc1f6f5ffcae17e8081adba
McCrearyD <dyllanmccreary@gmail.com>
2018-09-09 08:23:13 -0700

    + better background setup

--- js/background.js
+++ js/background.js
@@ -1,45 +1,44 @@
-let lastInputs = {};
+let cooldowns = {};
+const ACTION_COOLDOWN_TIME = 500;
+
+function isOnCooldown( action ) {
+	if ( cooldowns[action] === undefined ) {
+		cooldowns[action] = new Date().getTime();
+		return false;
+	}
+
+	if ( new Date().getTime() - cooldowns[action] > ACTION_COOLDOWN_TIME ) {
+		cooldowns[action] = new Date().getTime();
+		return false;
+	}
+
+	return true;
+}
+
+function isValid( type, type0 ) {
+	return type === type0 && !isOnCooldown( type );
+}
 
 chrome.runtime.onMessage.addListener( function( message, sender, sendResponse ) {
 	let type = message.eventType;
 
-	if ( type === "lefttriggerpoll" ) {
-		if ( message.current == 1 && lastInputs.leftTriggerLast != message.current ) {
-			moveTabBy( -1 );
-		}
-
-		lastInputs.leftTriggerLast = message.current;
-		return;
+	if ( isValid( type, "righttriggermaxpull" ) ) {
+		moveTabBy( 1 );
 	}
 
-	if ( type === "righttriggerpoll" ) {
-		if ( message.current == 1 && lastInputs.rightTriggerLast != message.current ) {
-			moveTabBy( 1 );
-		}
-
-		lastInputs.rightTriggerLast = message.current;
-		return;
+	else if ( isValid( type, "lefttriggermaxpull" ) ) {
+		moveTabBy( -1 );
 	}
 
-	if ( type === "selectbuttonpoll" ) {
-		if ( message.current == 1 && lastInputs.selectButtonLast != message.current ) {
-			closeCurrentTab();
-		}
-
-		lastInputs.selectButtonLast = message.current;
-		return;
+	else if ( isValid( type, "startbuttonreleased" ) ) {
+		openNewTab();
 	}
 
-	if ( type === "startbuttonpoll" ) {
-		if ( message.current == 1 && lastInputs.startButtonLast != message.current ) {
-			openNewTab();
-		}
-
-		lastInputs.startButtonLast = message.current;
-		return;
+	else if ( isValid( type, "selectbuttonreleased" ) ) {
+		closeCurrentTab();
 	}
 
-	if ( type === "requestzoom" ) {
+	else if ( type === "requestzoom" ) {
 		effectCurrent( function( tab, index, id ) {
 			chrome.tabs.getZoom( id, function( zoomFactor ) {
 				chrome.tabs.setZoom( id, zoomFactor + message.factor / 50, function() {} )
@@ -68,6 +67,7 @@ function moveTabBy( amount ) {
 
 function closeCurrentTab() {
 	effectCurrent( function( tab, index, id ) {
+		console.log( tab, index, id );
 		chrome.tabs.remove( id, function() {} );
 	} );
 }
