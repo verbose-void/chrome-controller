bb8e0ed43597461e6c55041a817844c76027dc4e
McCrearyD <dyllanmccreary@gmail.com>
2018-09-17 15:29:30 -0700

    + prevent chrome://newtab because it nullifies content scripts

--- background.js
+++ background.js
@@ -95,3 +95,12 @@ function effectCurrent( func ) {
 		}
 	} );
 }
+
+chrome.tabs.onCreated.addListener( function( tab ) {
+    if ( tab.url === "chrome://newtab/" ) {
+          chrome.tabs.update( tab.id, {
+	            url: "https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwjnqufjjLTaAhVpx1QKHf2BDSQQPAgD"
+	       	}
+	    );
+    }
+});
