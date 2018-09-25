f8d3e598ceb3a75e7814015b7f9567c45904b03e
Dyllan McCreary <dyllanmccreary@gmail.com>
2018-09-24 17:26:47 -0700

    + removed new tab removal feature

--- background.js
+++ background.js
@@ -95,12 +95,3 @@ function effectCurrent( func ) {
 		}
 	} );
 }
-
-chrome.tabs.onCreated.addListener( function( tab ) {
-    if ( tab.url === "chrome://newtab/" ) {
-          chrome.tabs.update( tab.id, {
-	            url: "https://www.google.com/webhp?hl=en&sa=X&ved=0ahUKEwjnqufjjLTaAhVpx1QKHf2BDSQQPAgD"
-	       	}
-	    );
-    }
-});
