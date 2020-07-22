84f93a335ba3c9c97cef13117c0dcebb3905f9e1
Tristan Tarpley <36747395+tristan-kenneth-tarpley@users.noreply.github.com>
2020-07-22 13:07:54 -0500

    Removed trailing slash

--- src/globals.js
+++ src/globals.js
@@ -2,6 +2,6 @@ const production = false;
 export default {
     production,
     apiUrl: production
-        ? 'https://chrome-controller-api.herokuapp.com/'
+        ? 'https://chrome-controller-api.herokuapp.com'
         : 'http://127.0.0.1:5000'
 }
